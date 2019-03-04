dd3ad61ed0159cf580aa8a1c8dc32b8c4d76fa3a
Hannes Nel <Hannes.Nel@windowslive.com>
2019-03-04 16:22:28 +1300

    Created tests and implementations

--- Python/V2/Echo/__init__.py
+++ Python/V2/Echo/__init__.py
@@ -1,24 +1,24 @@
 import logging
-
 import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
+    
+    try:
+        json_body = req.get_json()
+    except ValueError:
+        json_body = {}
+    
+    expected_status_code = __load_value(req, json_body, 'result')
+    expected_message = __load_value(req, json_body, 'message')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    return func.HttpResponse(body=expected_message,status_code=expected_status_code)
 
-    if name:
-        return func.HttpResponse(f"Hello {name}!")
-    else:
-        return func.HttpResponse(
-             "Please pass a name on the query string or in the request body",
-             status_code=400
-        )
+def __load_value(req, json_body, variable_name):
+    variable_value = req.params.get(variable_name)
+
+    if variable_value is None and variable_name in json_body.keys():
+        variable_value = json_body[variable_name]
+    return variable_value
+    
--- Python/V2/Tests/EchoTest.py
+++ Python/V2/Tests/EchoTest.py
@@ -1,12 +1,45 @@
 import unittest
 import Echo
 import azure.functions as func
+import json
 
 class EchoTest(unittest.TestCase):
-    def test_HelloWorld(self):
-        request = func.HttpRequest(method= "POST",url= "http://myfunction.com/api/echo",params=dict(name="James") ,body= bytearray())
+    TEST_URL = "http://myfunction.com/api/echo"
+    EXPECTED_CODE = 204
+    EXPECTED_MESSAGE = "test message"
+
+    def test_returns_defaults_in_result_when_no_parameters_provided(self):        
+        request = func.HttpRequest(method= "POST",url= EchoTest.TEST_URL,body= bytearray())
+
         response = Echo.main(request)
-        self.assertEquals(response.get_body(),bytearray("Hello James!","utf-8"))
+
+        self.assertEquals(response.status_code,200)
+        self.assertEqual(response.get_body(), bytearray())
+
+    def test_returns_query_parameters_in_result(self):
+        
+
+        request = func.HttpRequest(method= "POST",
+            url= EchoTest.TEST_URL,
+            params=dict(message=EchoTest.EXPECTED_MESSAGE,result=EchoTest.EXPECTED_CODE), 
+            body= bytearray())
+
+        response = Echo.main(request)
+
+        self.assertEquals(response.status_code,EchoTest.EXPECTED_CODE)
+        self.assertEqual(response.get_body(), bytearray(EchoTest.EXPECTED_MESSAGE,"utf-8"))
+
+    def test_returns_body_in_result(self):
+        
+        body = {'message':EchoTest.EXPECTED_MESSAGE, 'result':EchoTest.EXPECTED_CODE}
+        request = func.HttpRequest(method= "POST",
+            url= EchoTest.TEST_URL,
+            body= bytearray(json.dumps(body), "utf-8"))
+
+        response = Echo.main(request)
+
+        self.assertEquals(response.status_code,EchoTest.EXPECTED_CODE)
+        self.assertEqual(response.get_body(), bytearray(EchoTest.EXPECTED_MESSAGE,"utf-8"))
 
 if __name__ == '__main__':
     unittest.main()
